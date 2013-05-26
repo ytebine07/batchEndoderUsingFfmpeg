f6d28f354303f2df58516e29f66303e6cd13a2e9
ytebine07 <ytebine07@gmail.com>
2013-05-26 11:46:40 +0900

    変換後のファイル名変更対応

--- main.sh
+++ main.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 #--------------------------------
 #   ヨーヨー動画変換シェル
+#
+#   このシェルと同一ディレクトリにあるMOVファイルを変換する。
+#   起動時に同一ディレクトリにあるMOVファイル一覧を取得し、
+#   1個ずつ変換していくが、途中で追加されたファイルが有ったら、
+#   それを見つけ、変換する作りのはず。
 #--------------------------------
-DOING_FILE_NAME='doing.txt'
-DONE='./originalFile'
+DOING_FILE_NAME='doing.txt'     #動作中を判断するファイル名
+DONE='./originalFile'           #変換後にオリジナル動画ファイルをしまう場所
+TO_DIR='/Users/shrimp_yfa54/Desktop/'             #変換された動画ファイル名の出力場所
+                                #最後に / をつけること。
 
 # 動作中を判定するファイルを作成する
 # ファイルの有る間は、動作をし続けることになる。
@@ -34,13 +41,14 @@ do
         mv $FILE $PROCESSING_FILE_NAME
 
         #ココでエンコード
-        `./encode.sh ${PROCESSING_FILE_NAME} ~/Desktop`
-
-        #エンコード後のファイル名を変更する
-        mv ~/Desktop/${PROCESSING_FILE_NAME_NO_EXT}.mp4 ~/Desktop/${ORIGIN_FILE_NAME_NO_EXT}.mp4
+        ./encode.sh ${PROCESSING_FILE_NAME} ${TO_DIR}
 
         #元ファイルを置き場に移動
         mv $PROCESSING_FILE_NAME $DONE/$ORIGIN_FILE_NAME
+
+        #エンコード後のファイル名を変更する
+        mv ${TO_DIR}${PROCESSING_FILE_NAME_NO_EXT}.mp4 ${TO_DIR}${ORIGIN_FILE_NAME_NO_EXT}.mp4
+
     done
     echo "waiting"
     sleep 1
